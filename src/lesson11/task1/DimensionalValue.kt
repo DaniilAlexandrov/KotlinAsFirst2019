62d96eef15cdab8c77274a5d0bd94c55b7ff33eb
WorryWarrior <worrywarrior1@gmail.com>
2019-12-29 19:38:07 +0300

    Adjustments.

--- src/lesson11/task1/DimensionalValue.kt
+++ src/lesson11/task1/DimensionalValue.kt
@@ -16,62 +16,101 @@ package lesson11.task1
  * - либо соответствовать одной из приставок, к которой приписана сама размерность (Km, Kg, mm, mg)
  * - во всех остальных случаях следует бросить IllegalArgumentException
  */
-class DimensionalValue(value: Double, dimension: String) : Comparable<DimensionalValue> {
+
+fun valueDeterminant(s: String) = Regex("""\d+(\.\d+)?""").find(s)!!.value.toDouble()
+
+fun prefixDeterminant(s: String) = Regex("""[Km]?[mg]""").find(s)!!.value
+
+class DimensionalValue(private val actualValue: Double, private val actualDimension: String) :
+    Comparable<DimensionalValue> {
+
     /**
      * Величина с БАЗОВОЙ размерностью (например для 1.0Kg следует вернуть результат в граммах -- 1000.0)
      */
-    val value: Double get() = TODO()
+    val value: Double
+        get() {
+            return if (actualDimension.length == 1) actualValue
+            else actualValue * if (actualDimension.first() == 'K')
+                DimensionPrefix.KILO.multiplier else
+                DimensionPrefix.MILLI.multiplier
+
+
+        }
 
     /**
      * БАЗОВАЯ размерность (опять-таки для 1.0Kg следует вернуть GRAM)
      */
-    val dimension: Dimension get() = TODO()
+    val dimension: Dimension
+        get() {
+            return if (actualDimension.last() == 'm')
+                Dimension.METER
+            else Dimension.GRAM
+        }
 
     /**
      * Конструктор из строки. Формат строки: значение пробел размерность (1 Kg, 3 mm, 100 g и так далее).
      */
-    constructor(s: String) : this(TODO(), TODO())
+    constructor(s: String) : this(valueDeterminant(s), prefixDeterminant(s))
 
     /**
      * Сложение с другой величиной. Если базовая размерность разная, бросить IllegalArgumentException
      * (нельзя складывать метры и килограммы)
      */
-    operator fun plus(other: DimensionalValue): DimensionalValue = TODO()
+    operator fun plus(other: DimensionalValue): DimensionalValue {
+        require(dimension == other.dimension)
+        return DimensionalValue(value + other.value, dimension.abbreviation)
+    }
 
     /**
      * Смена знака величины
      */
-    operator fun unaryMinus(): DimensionalValue = TODO()
+    operator fun unaryMinus(): DimensionalValue = DimensionalValue(-value, dimension.abbreviation)
 
     /**
      * Вычитание другой величины. Если базовая размерность разная, бросить IllegalArgumentException
      */
-    operator fun minus(other: DimensionalValue): DimensionalValue = TODO()
+    operator fun minus(other: DimensionalValue): DimensionalValue = this + other.unaryMinus()
 
     /**
      * Умножение на число
      */
-    operator fun times(other: Double): DimensionalValue = TODO()
+    operator fun times(other: Double): DimensionalValue = DimensionalValue(value * other, dimension.abbreviation)
 
     /**
      * Деление на число
      */
-    operator fun div(other: Double): DimensionalValue = TODO()
+    operator fun div(other: Double): DimensionalValue {
+        require(other != 0.0)
+        return DimensionalValue(value / other, dimension.abbreviation)
+    }
 
     /**
      * Деление на другую величину. Если базовая размерность разная, бросить IllegalArgumentException
      */
-    operator fun div(other: DimensionalValue): Double = TODO()
+    operator fun div(other: DimensionalValue): Double {
+        require(dimension == other.dimension)
+        return value / other.value
+    }
 
     /**
      * Сравнение на равенство
      */
-    override fun equals(other: Any?): Boolean = TODO()
+    override fun equals(other: Any?): Boolean =
+        other is DimensionalValue && dimension == other.dimension && value == other.value
 
     /**
      * Сравнение на больше/меньше. Если базовая размерность разная, бросить IllegalArgumentException
      */
-    override fun compareTo(other: DimensionalValue): Int = TODO()
+    override fun compareTo(other: DimensionalValue): Int {
+        require(dimension == other.dimension)
+        return when {
+            value > other.value -> 1
+            value < other.value -> -1
+            else -> 0
+        }
+    }
+
+    override fun hashCode(): Int = javaClass.hashCode()
 }
 
 /**
