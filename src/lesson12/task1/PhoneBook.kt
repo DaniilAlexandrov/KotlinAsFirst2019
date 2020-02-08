4698c029e2f67d875906e77f5bb4c3588912ff93
WorryWarrior <worrywarrior1@gmail.com>
2020-02-08 19:47:35 +0300

    Presumably done.

--- src/lesson12/task1/PhoneBook.kt
+++ src/lesson12/task1/PhoneBook.kt
@@ -18,13 +18,22 @@ package lesson12.task1
  * Класс должен иметь конструктор по умолчанию (без параметров).
  */
 class PhoneBook {
+    private val book = mutableMapOf<String, Set<String>>()
+    private val nameTemplate = Regex("""([A-ZА-ЯЁ][a-zа-яё]*) [A-ZА-ЯЁ][a-zа-я]*""")
     /**
      * Добавить человека.
      * Возвращает true, если человек был успешно добавлен,
      * и false, если человек с таким именем уже был в телефонной книге
      * (во втором случае телефонная книга не должна меняться).
      */
-    fun addHuman(name: String): Boolean = TODO()
+    fun addHuman(name: String): Boolean {
+        require(name.matches(nameTemplate))
+        if (name in book) {
+            return false
+        }
+        book[name] = mutableSetOf()
+        return true
+    }
 
     /**
      * Убрать человека.
@@ -32,7 +41,14 @@ class PhoneBook {
      * и false, если человек с таким именем отсутствовал в телефонной книге
      * (во втором случае телефонная книга не должна меняться).
      */
-    fun removeHuman(name: String): Boolean = TODO()
+    fun removeHuman(name: String): Boolean {
+        require(name.matches(nameTemplate))
+        if (name in book) {
+            book.remove(name)
+            return true
+        }
+        return false
+    }
 
     /**
      * Добавить номер телефона.
@@ -41,7 +57,19 @@ class PhoneBook {
      * либо у него уже был такой номер телефона,
      * либо такой номер телефона зарегистрирован за другим человеком.
      */
-    fun addPhone(name: String, phone: String): Boolean = TODO()
+    fun addPhone(name: String, phone: String): Boolean {
+        for ((human, numbers) in book) {
+            require(name in book)
+            if (phone in numbers) {
+                return false
+            }
+            if (human == name) {
+                book[name] = book[name]!! + phone
+                return true
+            }
+        }
+        return false
+    }
 
     /**
      * Убрать номер телефона.
@@ -49,24 +77,52 @@ class PhoneBook {
      * и false, если человек с таким именем отсутствовал в телефонной книге
      * либо у него не было такого номера телефона.
      */
-    fun removePhone(name: String, phone: String): Boolean = TODO()
+    fun removePhone(name: String, phone: String): Boolean {
+        require(name in book)
+        if (phone in book[name]!!) {
+            book[name] = book[name]!! - phone
+            return true
+        }
+        return false
+    }
 
     /**
      * Вернуть все номера телефона заданного человека.
      * Если этого человека нет в книге, вернуть пустой список
      */
-    fun phones(name: String): Set<String> = TODO()
+    fun phones(name: String): Set<String> {
+        require(name.matches(nameTemplate))
+        if (name in book) {
+            return book[name]!!
+        }
+        return emptySet()
+    }
 
     /**
      * Вернуть имя человека по заданному номеру телефона.
      * Если такого номера нет в книге, вернуть null.
      */
-    fun humanByPhone(phone: String): String? = TODO()
+    fun humanByPhone(phone: String): String? {
+        for ((human, numbers) in book) {
+            if (phone in numbers) {
+                return human
+            }
+        }
+        return null
+    }
 
     /**
      * Две телефонные книги равны, если в них хранится одинаковый набор людей,
      * и каждому человеку соответствует одинаковый набор телефонов.
      * Порядок людей / порядок телефонов в книге не должен иметь значения.
      */
-    override fun equals(other: Any?): Boolean = TODO()
+    override fun equals(other: Any?): Boolean {
+        require(other is PhoneBook)
+        for ((name, phones) in book) {
+            return other.book[name] == phones
+        }
+        return false
+    }
+
+    override fun hashCode(): Int = javaClass.hashCode()
 }
